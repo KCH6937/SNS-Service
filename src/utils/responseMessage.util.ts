ae7b67335696a81cb11c535b1fb2c1bff60f07ee
KCH6937 <adasddasd12@naver.com>
2022-07-22 13:29:07 +0900

    [Feat] response message 추가

---
+++ src/utils/responseMessage.util.ts
@@ -0,0 +1,19 @@
+const message = {
+  SUCCESS: '성공',
+  NO_CONTENT: '콘텐츠 없음',
+  BAD_REQUEST: '잘못된 요청',
+  UNAUTHORIZED: '인증 실패',
+  FORBIDDEN: 'Forbidden',
+  NOT_FOUND: '존재하지 않는 자원',
+  INTERNAL_SERVER_ERROR: '서버 내부 오류',
+
+  ENTERED_BOSSRAID: '현재 진행중인 보스레이드',
+  NOT_USER_RAIDRECORDID: '유저의 raidRecordId 아님',
+  ALREADY_END_RAIDRECORDID: '이미 종료된 raidRecordId',
+  EXPIRED_RAIDRECORDID: '이미 만료된 raidRecordId',
+
+  INVALID_USERID: '존재하지 않는 userId',
+  INVALID_RECORDID: '존재하지 않는 recordId',
+};
+
+export default message;

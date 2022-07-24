f36d3b8cc5dc3a7d00af0cc09d03034f59623d55
KCH6937 <adasddasd12@naver.com>
2022-07-24 23:07:21 +0900

    [Refactor] 불필요한 코드 추가

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createConnection } from 'typeorm';
 import morgan from 'morgan';
-import 'dotenv/config';
+import { createConnection } from 'typeorm';
+import './utils/env.util';
 
 // Import Routers
 import { usersRouter } from './routes/users.route';
+import { boardsRouter } from './routes/boards.route';
+
 // Connect typeORM mysql
 createConnection()
   .then(() => {
@@ -19,10 +21,12 @@ const app = express();
 
 // middlewares
 app.set('port', process.env.PORT || 3000);
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
 // Routes
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/boards', boardsRouter);
 
 export default app;

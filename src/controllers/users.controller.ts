1997de151e151cc929fef380a8d601d12a094e7c
KCH6937 <adasddasd12@naver.com>
2022-07-24 23:07:54 +0900

    [Feat] 회원가입, 로그인, 회원탈퇴 추가

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,11 +2,15 @@ import { Request, Response } from 'express';
 import userService from '../services/users.service';
 
 const signUp = async (req: Request, res: Response) => {
-  return res.status(500);
+  const { email, password } = req.body;
+  const [statusCode, result] = await userService.signUp(email, password);
+  return res.status(statusCode as number).send(result);
 };
 
 const signIn = async (req: Request, res: Response) => {
-  return res.status(500);
+  const { email, password } = req.body;
+  const [statusCode, result] = await userService.signIn(email, password);
+  return res.status(statusCode as number).send(result);
 };
 
 const signOut = async (req: Request, res: Response) => {
@@ -14,7 +18,9 @@ const signOut = async (req: Request, res: Response) => {
 };
 
 const withdraw = async (req: Request, res: Response) => {
-  return res.status(500);
+  const { userId } = req.body;
+  const [statusCode, result] = await userService.withdraw(userId as number);
+  return res.status(statusCode as number).send(result);
 };
 
 export default {

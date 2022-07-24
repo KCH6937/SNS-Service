9208ce90fbf2a48a391ded6be8c27af8c047f3a9
KCH6937 <adasddasd12@naver.com>
2022-07-24 23:11:48 +0900

    [Feat] user 라우터 기능추가

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express';
 import usersController from '../controllers/users.controller';
+import authJWT from '../middlewares/auth.middlewares';
+
 export const usersRouter = Router();
 
-usersRouter.post('/signup', usersController.signUp);
-usersRouter.post('/signin', usersController.signIn);
-usersRouter.get('/signout', usersController.signOut);
+usersRouter.post('/signup', usersController.signUp); // 회원가입
+
+usersRouter.post('/signin', usersController.signIn); // 로그인
+
+usersRouter.post('/signout', authJWT, usersController.signOut); // 로그아웃
+
+usersRouter.patch('/edit', authJWT, usersController.signIn); // 회원정보 수정
+
+usersRouter.delete('/withdraw', authJWT, usersController.withdraw); // 회원탈퇴
